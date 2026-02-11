c6a68a3bf020dc78e2d398f1db9ca4304d18066c
Ahmad <ma7hh29261@gmail.com>
2026-02-11 15:49:48 +0500

    Adding new module css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import '@/app/ui/global.css'
 
 export default function Page() {
   return (
@@ -16,6 +17,9 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
+            <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/>
           </p>
           <Link
             href="/login"
